01a7999327f9460a8f6f11aa8cc01e97a1a7e09a
bekyiu <651731457@qq.com>
2019-05-23 14:07:56 +0800

    mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,45 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// pseudo code:
+// sum = 0
+// i = 0
+// (LOOP)
+// 		if i >= R1 goto STOP
+//		sum = sum + R0
+//		i = i + 1;
+// (STOP)
+// R2 = sum
+
+@sum
+M = 0
+@i
+M = 0
+
+(LOOP)
+	@i
+	D = M
+	@R1
+	D = D - M
+	@STOP
+	D; JGE
+	@sum
+	D = M
+	@R0
+	D = D + M
+	@sum
+	M = D
+	@i
+	M = M + 1
+	@LOOP
+	0; JMP
+(STOP)
+
+@sum
+D = M
+@R2
+M = D
+(END)
+@END
+0; JMP
