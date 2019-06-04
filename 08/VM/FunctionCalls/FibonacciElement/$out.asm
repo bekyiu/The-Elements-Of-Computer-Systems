a9c6a18f4b6136948fba0ab37461f7c1c20683da
bekyiu <651731457@qq.com>
2019-06-04 13:59:00 +0800

    可以对一个目录下的所有.vm文件进行翻译, 最终生成一个.asm文件

---
+++ 08/VM/FunctionCalls/FibonacciElement/$out.asm
@@ -0,0 +1,440 @@
+(Main.fibonacci)
+@0
+D = A
+@times
+M = D
+(LOOP0)
+@times
+D = M
+@END0
+D; JEQ
+@0
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@times
+M = M - 1
+@LOOP0
+0; JMP
+(END0)
+@0
+D = A
+@ARG
+A = D + M
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@2
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+M = M -1
+A = M
+D = M
+@SP
+M = M - 1
+A = M
+D = M - D
+@LT1
+D; JLT
+@SP
+A = M
+M = 0
+@END1
+0; JMP
+(LT1)
+@SP
+A = M
+M = -1
+(END1)
+@SP
+M = M + 1
+@SP
+M = M - 1
+A = M
+D = M
+@IF_TRUE
+D; JNE
+@IF_FALSE
+0; JMP
+(IF_TRUE)
+@0
+D = A
+@ARG
+A = D + M
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@LCL
+D = M
+@frame
+M = D
+@5
+D = D - A
+A = D
+D = M
+@ret
+M = D
+@SP
+M = M - 1
+A = M
+D = M
+@ARG
+A = M
+M = D
+@ARG
+D = M + 1
+@SP
+M = D
+@frame
+D = M
+D = D - 1
+A = D
+D = M
+@THAT
+M = D
+@frame
+D = M
+@2
+D = D - A
+A = D
+D = M
+@THIS
+M = D
+@frame
+D = M
+@3
+D = D - A
+A = D
+D = M
+@ARG
+M = D
+@frame
+D = M
+@4
+D = D - A
+A = D
+D = M
+@LCL
+M = D
+@ret
+A = M
+0; JMP
+(IF_FALSE)
+@0
+D = A
+@ARG
+A = D + M
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@2
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+M = M - 1
+A = M
+D = M
+@SP
+M = M - 1
+A = M
+D = M - D
+M = D
+@SP
+M = M + 1
+@RETURN-ADDRESS2
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@LCL
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@ARG
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@THIS
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@THAT
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+D = M
+@1
+D = D - A
+@5
+D = D - A
+@ARG
+M = D
+@SP
+D = M
+@LCL
+M = D
+@Main.fibonacci
+0; JMP
+(RETURN-ADDRESS2)
+@0
+D = A
+@ARG
+A = D + M
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@1
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+M = M - 1
+A = M
+D = M
+@SP
+M = M - 1
+A = M
+D = M - D
+M = D
+@SP
+M = M + 1
+@RETURN-ADDRESS3
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@LCL
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@ARG
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@THIS
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@THAT
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+D = M
+@1
+D = D - A
+@5
+D = D - A
+@ARG
+M = D
+@SP
+D = M
+@LCL
+M = D
+@Main.fibonacci
+0; JMP
+(RETURN-ADDRESS3)
+@SP
+M = M - 1
+A = M
+D = M
+@SP
+M = M - 1
+A = M
+D = M + D
+M = D
+@SP
+M = M + 1
+@LCL
+D = M
+@frame
+M = D
+@5
+D = D - A
+A = D
+D = M
+@ret
+M = D
+@SP
+M = M - 1
+A = M
+D = M
+@ARG
+A = M
+M = D
+@ARG
+D = M + 1
+@SP
+M = D
+@frame
+D = M
+D = D - 1
+A = D
+D = M
+@THAT
+M = D
+@frame
+D = M
+@2
+D = D - A
+A = D
+D = M
+@THIS
+M = D
+@frame
+D = M
+@3
+D = D - A
+A = D
+D = M
+@ARG
+M = D
+@frame
+D = M
+@4
+D = D - A
+A = D
+D = M
+@LCL
+M = D
+@ret
+A = M
+0; JMP
+(Sys.init)
+@0
+D = A
+@times
+M = D
+(LOOP4)
+@times
+D = M
+@END4
+D; JEQ
+@0
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@times
+M = M - 1
+@LOOP4
+0; JMP
+(END4)
+@4
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@RETURN-ADDRESS5
+D = A
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@LCL
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@ARG
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@THIS
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@THAT
+D = M
+@SP
+A = M
+M = D
+@SP
+M = M + 1
+@SP
+D = M
+@1
+D = D - A
+@5
+D = D - A
+@ARG
+M = D
+@SP
+D = M
+@LCL
+M = D
+@Main.fibonacci
+0; JMP
+(RETURN-ADDRESS5)
+(WHILE)
+@WHILE
+0; JMP
--- 08/VM/src/main/java/bekyiu/VM.java
+++ 08/VM/src/main/java/bekyiu/VM.java
@@ -9,12 +9,94 @@ public class VM
 
     public static void main(String[] args)
     {
-
-        //FunctionCalls/SimpleFunction/SimpleFunction.vm
-        new VM().build("FunctionCalls/SimpleFunction/", "SimpleFunction.vm");
+        VM vm = new VM();
+        // vm.buildSingle("FunctionCalls/SimpleFunction/", "SimpleFunction.vm")
+        vm.build("FunctionCalls/FibonacciElement/");
     }
 
-    public void build(String path, String fileName)
+    // 先将每个.vm都编译出一个.asm, 最后再合并为$out.asm
+    public void build(String path)
+    {
+        File directory = new File(path);
+        if (directory.isDirectory())
+        {
+            // 先清除所有的.asm文件
+            deleteFiles(directory, "asm");
+            //翻译所有.vm文件
+            translateAll(directory);
+            //merge并删除中间过程的.asm文件
+            mergeAndDelete(directory, "asm");
+        }
+    }
+
+    // 删除directory目录下, 指定后缀的文件
+    private void deleteFiles(File directory, String suffix)
+    {
+        File[] asmFiles = directory.listFiles((dir, name) ->
+        {
+            String[] split = name.split("\\.");
+            return split[1].equals(suffix);
+        });
+        for (File file : asmFiles)
+        {
+            file.delete();
+        }
+    }
+
+    // 翻译directory目录下的所有.vm文件
+    private void translateAll(File directory)
+    {
+        File[] vmFiles = directory.listFiles((dir, name) ->
+        {
+            String[] split = name.split("\\.");
+            return split[1].equals("vm");
+        });
+        for (File file : vmFiles)
+        {
+            translateSingle(directory.getPath(), file.getName());
+        }
+    }
+
+    // merge directory目录下 所有后缀为suffix的文件(一边merge一边删除)
+    // 最终生成$out.asm文件
+    private void mergeAndDelete(File directory, String suffix)
+    {
+        File[] files = directory.listFiles((dir, name) ->
+        {
+            String[] split = name.split("\\.");
+            return split[1].equals(suffix);
+        });
+
+        File out = new File(directory.getPath(), "$out.asm");
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        try
+        {
+            bw = new BufferedWriter(new FileWriter(out, true));
+            for (File file : files)
+            {
+                br = new BufferedReader(new FileReader(file));
+                String line = "";
+                while ((line = br.readLine()) != null)
+                {
+                    bw.write(line);
+                    bw.newLine();
+                }
+                close(br, null);
+                file.delete();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            close(br, bw);
+        }
+    }
+
+    public void translateSingle(String path, String fileName)
     {
         File vmFile = new File(path, fileName);
         File asmFile = new File(path, fileName.split("\\.")[0] + ".asm");
@@ -65,27 +147,27 @@ public class VM
         {
             asm = codeWriter.writePushPop(c);
         }
-        else if(type.equals(Parser.C_LABEL))
+        else if (type.equals(Parser.C_LABEL))
         {
             asm = codeWriter.writeLabel(c);
         }
-        else if(type.equals(Parser.C_GOTO))
+        else if (type.equals(Parser.C_GOTO))
         {
             asm = codeWriter.writeGoto(c);
         }
-        else if(type.equals(Parser.C_IF))
+        else if (type.equals(Parser.C_IF))
         {
             asm = codeWriter.writeIf(c);
         }
-        else if(type.equals(Parser.C_CALL))
+        else if (type.equals(Parser.C_CALL))
         {
             asm = codeWriter.writeCall(c);
         }
-        else if(type.equals(Parser.C_FUNCTION))
+        else if (type.equals(Parser.C_FUNCTION))
         {
             asm = codeWriter.writeFunction(c);
         }
-        else if(type.equals(Parser.C_RETURN))
+        else if (type.equals(Parser.C_RETURN))
         {
             asm = codeWriter.writeReturn();
         }
