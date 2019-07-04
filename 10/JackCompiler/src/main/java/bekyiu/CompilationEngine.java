905bb912e47ec87a48285098c1cd757f72613b4c
bekyiu <651731457@qq.com>
2019-07-04 12:30:19 +0800

    Expressionless版本大致功能写完, 还有一些 自身可以出现多次 的问题没有解决

--- 10/JackCompiler/src/main/java/bekyiu/CompilationEngine.java
+++ 10/JackCompiler/src/main/java/bekyiu/CompilationEngine.java
@@ -1,5 +1,7 @@
 package bekyiu;
 
+import jdk.nashorn.internal.parser.Token;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -93,6 +95,21 @@ public class CompilationEngine
         //如果声明了字段, 那么这个token应该是static或者field
         //如果没有声明字段, 则应该是function method constructor } (空的类)
         String token = tokens.element();
+        while (JackTokenizer.STATIC.equals(token) || JackTokenizer.FIELD.equals(token))
+        {
+            compileClassVarDec1();
+            //如果声明了字段, 那么这个token应该是static或者field
+            //如果没有声明字段, 则应该是function method constructor } (空的类)
+            token = tokens.element();
+        }
+    }
+
+    private void compileClassVarDec1() throws IOException
+    {
+        Queue<String> tokens = tokenizer.getTokens();
+        //如果声明了字段, 那么这个token应该是static或者field
+        //如果没有声明字段, 则应该是function method constructor } (空的类)
+        String token = tokens.element();
         if (JackTokenizer.STATIC.equals(token) || JackTokenizer.FIELD.equals(token))
         {
             tokens.remove();
@@ -100,8 +117,7 @@ public class CompilationEngine
             write("<keyword>" + token + "</keyword>");
             // type, 基本类型和引用类型
             token = tokens.remove();
-            if (JackTokenizer.INT.equals(token) || JackTokenizer.BOOLEAN.equals(token) ||
-                    JackTokenizer.CHAR.equals(token))
+            if (typeCheck(token))
             {
                 write("<keyword>" + token + "</keyword>");
             }
@@ -149,8 +165,7 @@ public class CompilationEngine
             write("<keyword>" + token + "</keyword>");
             // 函数的返回值, void, 基本类型, 引用
             token = tokens.remove();
-            if (JackTokenizer.INT.equals(token) || JackTokenizer.BOOLEAN.equals(token) ||
-                    JackTokenizer.CHAR.equals(token) || JackTokenizer.VOID.equals(token))
+            if (typeCheck(token) || JackTokenizer.VOID.equals(token))
             {
                 write("<keyword>" + token + "</keyword>");
             }
@@ -185,8 +200,7 @@ public class CompilationEngine
 
         while (!")".equals(token))
         {
-            if (JackTokenizer.INT.equals(token) || JackTokenizer.BOOLEAN.equals(token) ||
-                    JackTokenizer.CHAR.equals(token))
+            if (typeCheck(token))
             {
                 write("<keyword>" + token + "</keyword>");
             }
@@ -211,58 +225,314 @@ public class CompilationEngine
     }
 
     // 编译函数体{...}
-    public void compileSubroutineBody()
+    public void compileSubroutineBody() throws IOException
     {
+        Queue<String> tokens = tokenizer.getTokens();
+        // {
+        String token = tokens.remove();
+        if ("{".equals(token))
+        {
+            write("<subroutineBody>");
+            write("<symbol>" + token + "</symbol>");
+            compileVarDec();
+            compileStatements();
+            // }
+            token = tokens.remove();
+            write("<symbol>" + token + "</symbol>");
+            write("</subroutineBody>");
+        }
+        else
+        {
+            throw new RuntimeException("期望是 {, 实际上是" + token);
+        }
+    }
+
+    public void compileVarDec() throws IOException
+    {
+        Queue<String> tokens = tokenizer.getTokens();
+        // var, 不是var(statements)
+        String token = tokens.element();
+        while (JackTokenizer.VAR.equals(token))
+        {
+            compileVarDec1();
+            token = tokens.element();
+        }
+    }
+
+    private void compileVarDec1() throws IOException
+    {
+        Queue<String> tokens = tokenizer.getTokens();
+        // 假设是 var
+        String token = tokens.remove();
+        write("<varDec>");
+        write("<keyword>" + token + "</keyword>");
+        // type
+        token = tokens.remove();
+        if (typeCheck(token))
+        {
+            write("<keyword>" + token + "</keyword>");
+        }
+        else
+        {
+            write("<identifier>" + token + "</identifier>");
+        }
+        // varName
+        token = tokens.remove();
+        write("<identifier>" + token + "</identifier>");
+        // , or ;
+        token = tokens.remove();
+        while (!token.equals(";"))
+        {
+            //,
+            write("<symbol>" + token + "</symbol>");
+            //type
+            token = tokens.remove();
+            if (typeCheck(token))
+            {
+                write("<keyword>" + token + "</keyword>");
+            }
+            else
+            {
+                write("<identifier>" + token + "</identifier>");
+            }
+            //varName
+            token = tokens.remove();
+            write("<identifier>" + token + "</identifier>");
+            token = tokens.remove();
+        }
+        //此时 token等于 ;
+        write("<symbol>" + token + "</symbol>");
+        write("</varDec>");
+    }
+
+    private boolean typeCheck(String token)
+    {
+        return JackTokenizer.INT.equals(token) || JackTokenizer.BOOLEAN.equals(token) ||
+                JackTokenizer.CHAR.equals(token);
+    }
+
+    public void compileStatements() throws IOException
+    {
+        write("<statements>");
+        Queue<String> tokens = tokenizer.getTokens();
+        // let if while do return }
+        String token = tokens.element();
+        if (JackTokenizer.LET.equals(token))
+        {
+            compileLet();
+        }
+        else if (JackTokenizer.IF.equals(token))
+        {
+            compileIf();
+        }
+        else if (JackTokenizer.WHILE.equals(token))
+        {
+            compileWhile();
+        }
+        else if (JackTokenizer.DO.equals(token))
+        {
+            compileDo();
+        }
+        else if (JackTokenizer.RETURN.equals(token))
+        {
+            compileReturn();
+        }
+        write("</statements>");
+    }
+
+    public void compileDo() throws IOException
+    {
+        Queue<String> tokens = tokenizer.getTokens();
+        // do
+        String token = tokens.remove();
+        write("<doStatement>");
+        write("<keyword>" + token + "</keyword>");
+        // fun(xx); or xx.fun(xx);
+        // funName, className, varName
+        token = tokens.remove();
+        //( or .
+        String nextToken = tokens.element();
+        if ("(".equals(nextToken))
+        {
+            write("<identifier>" + token + "</identifier>");
+            // (
+            token = tokens.remove();
+            write("<symbol>" + token + "</symbol>");
+            compileExpressionList();
+            // )
+            token = tokens.remove();
+            write("<symbol>" + token + "</symbol>");
+        }
+        else if (".".equals(nextToken))
+        {
+            write("<identifier>" + token + "</identifier>");
+            // .
+            token = tokens.remove();
+            write("<symbol>" + token + "</symbol>");
+            // funName
+            token = tokens.remove();
+            write("<identifier>" + token + "</identifier>");
+            // (
+            token = tokens.remove();
+            write("<symbol>" + token + "</symbol>");
+            compileExpressionList();
+            // )
+            token = tokens.remove();
+            write("<symbol>" + token + "</symbol>");
+
+        }
+        write("</doStatement>");
 
     }
 
-    public void compileVarDec()
+    public void compileLet() throws IOException
     {
+        Queue<String> tokens = tokenizer.getTokens();
+        // let
+        String token = tokens.remove();
+        write("<letStatement>");
+        write("<keyword>" + token + "</keyword>");
+        // varName
+        token = tokens.remove();
+        write("<identifier>" + token + "</identifier>");
+        // a = ...., a[1] = ...
+        token = tokens.remove();
+        if ("[".equals(token))
+        {
+            write("<symbol>" + token + "</symbol>");
+            compileExpression();
+            // ]
+            token = tokens.remove();
+            write("<symbol>" + token + "</symbol>");
+            // =
+            token = tokens.remove();
+        }
+        write("<symbol>" + token + "</symbol>");
+        // expression
+        // token = tokens.remove();
+        compileExpression();
+        // ;
+        token = tokens.remove();
+        write("<symbol>" + token + "</symbol>");
 
+        write("</letStatement>");
     }
 
-    public void compileStatements()
+    public void compileWhile() throws IOException
     {
-
+        Queue<String> tokens = tokenizer.getTokens();
+        // while
+        String token = tokens.remove();
+        write("<whileStatement>");
+        write("<keyword>" + token + "</keyword>");
+        // (
+        token = tokens.remove();
+        write("<symbol>" + token + "</symbol>");
+        compileExpression();
+        // )
+        token = tokens.remove();
+        write("<symbol>" + token + "</symbol>");
+        // {
+        token = tokens.remove();
+        write("<symbol>" + token + "</symbol>");
+        compileStatements();
+        // }
+        token = tokens.remove();
+        write("<symbol>" + token + "</symbol>");
+        write("</whileStatement>");
     }
 
-    public void compileDo()
+    public void compileReturn() throws IOException
     {
-
+        Queue<String> tokens = tokenizer.getTokens();
+        // return
+        String token = tokens.remove();
+        write("<returnStatement>");
+        write("<keyword>" + token + "</keyword>");
+        compileExpression();
+        // ;
+        token = tokens.remove();
+        write("<symbol>" + token + "</symbol>");
+        write("</returnStatement>");
     }
 
-    public void compileLet()
+    public void compileIf() throws IOException
     {
-
+        Queue<String> tokens = tokenizer.getTokens();
+        // if
+        String token = tokens.remove();
+        write("<ifStatement>");
+        write("<keyword>" + token + "</keyword>");
+        // (
+        token = tokens.remove();
+        write("<symbol>" + token + "</symbol>");
+        compileExpression();
+        // )
+        token = tokens.remove();
+        write("<symbol>" + token + "</symbol>");
+        // {
+        token = tokens.remove();
+        write("<symbol>" + token + "</symbol>");
+        compileStatements();
+        // }
+        token = tokens.remove();
+        write("<symbol>" + token + "</symbol>");
+        //s是否是else
+        token = tokens.element();
+        if (JackTokenizer.ELSE.equals(token))
+        {
+            // else
+            token = tokens.remove();
+            write("<keyword>" + token + "</keyword>");
+            // {
+            token = tokens.remove();
+            write("<symbol>" + token + "</symbol>");
+            compileStatements();
+            // }
+            token = tokens.remove();
+            write("<symbol>" + token + "</symbol>");
+        }
+        write("</ifStatement>");
     }
 
-    public void compileWhile()
+    //expressionless
+    public void compileExpression() throws IOException
     {
-
+        write("<expression>");
+        compileTerm();
+        write("</expression>");
     }
 
-    public void compileReturn()
+    public void compileTerm() throws IOException
     {
-
+        Queue<String> tokens = tokenizer.getTokens();
+        // if ( x )
+        String token = tokens.remove();
+        write("<term>");
+        write("<identifier>" + token + "</identifier>");
+        write("</term>");
     }
 
-    public void compileIf()
+    public void compileExpressionList() throws IOException
     {
-
-    }
-
-    public void compileExpression()
-    {
-
-    }
-
-    public void compileTerm()
-    {
-
-    }
-
-    public void compileExpressionList()
-    {
+        Queue<String> tokens = tokenizer.getTokens();
+        // ) or xx
+        String token = tokens.element();
+        if (!")".equals(token))
+        {
+            compileExpression();
+            //, or )
+            token = tokens.element();
+            while (!")".equals(token))
+            {
+                //,
+                token = tokens.remove();
+                write("<symbol>" + token + "</symbol>");
+//                token = tokens.remove();
+                compileExpression();
+                token = tokens.element();
+            }
+        }
 
     }
 }
